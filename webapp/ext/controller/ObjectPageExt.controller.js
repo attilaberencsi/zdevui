ee6ba02e8e392c0ee90f2f469722c17d44ccf20e
attilaberencsi <attilaberencsi@gmail.com>
2021-01-25 14:31:28 +0000

    SideEffects

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,5 +1,11 @@
 sap.ui.controller("cust.ca.devman.zcadevman.ext.controller.ObjectPageExt", {
     
+
+    /**
+     * Bound to the post event of the FeedInput to add comments
+     *
+     * @param {*} oEvent
+     */
     onAddCommentZC_Dev : function(oEvent) {         
         var oExtAPI = this.extensionAPI;
         var oTxCtrl = oExtAPI.getTransactionController();
